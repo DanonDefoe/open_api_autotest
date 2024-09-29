aa04f1a14c2e235088db49795239a6008058e579
Daniel Pichugin <daniel.pichugin@setinbox.com>
2024-09-30 02:48:34 +0400

    created the dto, steps, tests and etc

---
+++ dto/users_list.py
@@ -0,0 +1,10 @@
+from schematics.models import Model
+from schematics.types import IntType
+
+
+class ListUsers(Model):
+    page = IntType()
+    per_page =
+    total =
+    total_pages =
+    data =
