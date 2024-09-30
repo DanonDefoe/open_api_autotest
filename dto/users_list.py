013ad54baf6201ea00b36653aecd39291aa5ee3a
DanonDefoe <136860726+DanonDefoe@users.noreply.github.com>
2024-09-30 11:04:57 +0400

    create users list model

--- dto/users_list.py
+++ dto/users_list.py
@@ -1,10 +1,23 @@
-from schematics.models import Model
-from schematics.types import IntType
+from pydantic import BaseModel
 
 
-class ListUsers(Model):
-    page = IntType()
-    per_page =
-    total =
-    total_pages =
-    data =
+class UsersData(BaseModel):
+    id: int
+    email: str
+    first_name: str
+    last_name: str
+    avatar: str
+
+
+class UsersSupport(BaseModel):
+    url: str
+    text: str
+
+
+class ListUsersSchema(BaseModel):
+    page: int
+    per_page: int
+    total: int
+    total_pages: int
+    data: UsersData
+    support: UsersSupport
